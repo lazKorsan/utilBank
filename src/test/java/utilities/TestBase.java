a771b06f182e322df83cb0402fa61c02ac473505
lazKorsan <ahmeteldes2002@gmail.com>
2025-07-06 04:22:55 +0300

    driver kurulumu temiz

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class BeforeEach_AfterEach {
+    public static WebDriver createDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("force-device-scale-factor=1.0");
+        options.addArguments("high-dpi-support=1.0");
+        options.addArguments("--disable-notifications");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
+
+    public static void tearDown() {
+        if (Driver.driver != null) {
+            Driver.driver.quit();
+            Driver.driver = null;
+        }
+    }
+}
